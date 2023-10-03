98024ad1f602da2ffad0dc46bbededcf3e8303bd
FilJef <QvjLAaJ6bp4BnMB>
2023-10-03 12:35:42 +0100

    changes

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='mypackage',
+setup(name='Helpers',
 version='0.1',
 description='Testing installation of Package',
 url='#',
 author='auth',
 author_email='author@email.com',
 license='MIT',
-packages=['mypackage'],
+packages=['Helpers'],
 zip_safe=False)
